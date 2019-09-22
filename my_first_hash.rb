80c461feb14d1e42b5f6d55c4221fe5032ee3c8a
sarah <s.j.condon01@gmail.com>
2019-09-22 15:56:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,14 +32,10 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   
 
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+# return the shipping_manifest hash below
 
 end
